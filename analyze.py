27b00449e4a873764765ead1e3a88010950f6dcd
Yanay Zimroni <yanay03@gmail.com>
2021-02-27 15:20:41 +0200

    Add date & play count filters

--- analyze.py
+++ analyze.py
@@ -17,25 +17,17 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 
-def output_path(string):
-    """
-    Checks if a path doesn't exist
-    :param string: Path to check
-    :return: The path, if it doesn't exist, otherwise raises FileExistsError
-    """
-    if os.path.exists(string):
-        raise FileExistsError(string)
-    else:
-        return string
-
-
 def run():
     """
     Run the program
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--source", "-s", help="'MyData' folder from spotify data export", type=dir_path, required=True)
-    parser.add_argument("--output", "-o", help="Output file", type=output_path, required=True)
+    parser.add_argument("--source", "-s", help="'MyData' folder from Spotify data export", type=dir_path, required=True)
+    parser.add_argument("--output", "-o", help="Output file", type=str, required=True)
+    parser.add_argument("--from-date", "-d", help="Optional. Filter streaming history before this date (format: "
+                                                  "YYYY-MM-DD)", type=str)
+    parser.add_argument("--min-count", "-c", help="Optional. Filter songs below a specific play count", type=int)
+
     args = parser.parse_args()
 
     print("Scanning for streaming history files...")
@@ -44,21 +36,38 @@ def run():
 
     analyzer = SpotifyStreamingAnalyzer()
     for fn in files:
+        # Load each streaming history file as json
         with open(os.path.join(args.source, fn), mode="r", encoding="utf-8") as f:
             history = json.load(f)
             print("  == Loaded %i streaming records from %s" % (len(history), fn))
+            # Add streaming history records to analyzer
             analyzer.add_streaming_history(history)
 
-    total_length = len(analyzer.get_streaming_history())
+    analyzer.generate_data_frame()
+
+    # Filter streaming history by date
+    if args.from_date:
+        print("Filtering streaming history before %s" % args.from_date)
+        analyzer.filter_by_date(args.from_date)
+
+    total_length = analyzer.get_data_frame().size
     print("Total streaming records: %i" % total_length)
     if total_length == 0:
+        # If length is 0, we have no records to analyze
         print("Streaming history is empty")
         return
 
-    result = analyzer.analyze()
-    result.to_csv(args.output)
-    print("output written to %s" % args.output)
+    analyzer.analyze()
 
+    # Filter songs below specified play count
+    if args.min_count:
+        print("Filtering songs with play count less than %i" % args.min_count)
+        analyzer.filter_by_play_count(args.min_count)
+
+    result = analyzer.get_result()
+    # Save results to file
+    result.to_csv(args.output)
+    print("Output written to %s" % args.output)
 
 
 if __name__ == "__main__":
--- spotify_streaming_analyzer.py
+++ spotify_streaming_analyzer.py
@@ -5,6 +5,8 @@ class SpotifyStreamingAnalyzer:
 
     def __init__(self):
         self.streaming = []
+        self.data_frame = None
+        self.grouped_data = None
 
     def add_streaming_history(self, history):
         """
@@ -20,19 +22,53 @@ class SpotifyStreamingAnalyzer:
         """
         return self.streaming
 
+    def get_data_frame(self):
+        """
+        Get the dataframe used for analyzing
+        :return: Current dataframe
+        """
+        return self.data_frame
+
+    def generate_data_frame(self):
+        """
+        Generate DataFrame from streaming history
+        """
+        self.data_frame = pd.DataFrame(self.streaming)
+
+    def filter_by_date(self, from_date):
+        """
+        Filter streaming history before a specific date
+        :param from_date: The date to filter from (fromat: YYYY-MM-DD)
+        """
+        filtered = self.data_frame[self.data_frame["endTime"] > from_date]
+        self.data_frame = filtered
+
     def analyze(self):
         """
         Analyze streaming history
-        :return: DataFrame of the analyzed data
         """
         # Create pandas DataFrame from streaming list
-        df = pd.DataFrame(self.streaming)
         # Group by tracks (identified by artist and track names)
-        gr = df.groupby(["artistName", "trackName"])
+        self.grouped_data = self.data_frame.groupby(["artistName", "trackName"])
         # Aggregate results
-        # Values are deviced by 1000 to convert ms to seconds
-        gr = gr.agg({"msPlayed": ["count", lambda x: int(x.sum() / 1000), lambda x: int((x.sum() / x.count()) / 1000)],
-                     "endTime": ["min", "max"]})
-        # Rename colums
-        gr.columns = ["Count", "TotalSecPlayed", "AvgSecPerPlay", "FirstPlayed", "LastPlayed"]
-        return gr
+        # Values are divided by 1000 to convert ms to seconds
+        self.grouped_data = self.grouped_data.agg(
+            {"msPlayed": ["count", lambda x: int(x.sum() / 1000), lambda x: int((x.sum() / x.count()) / 1000)],
+             "endTime": ["min", "max"]})
+        # Rename columns
+        self.grouped_data.columns = ["Count", "TotalSecPlayed", "AvgSecPerPlay", "FirstPlayed", "LastPlayed"]
+
+    def filter_by_play_count(self, count):
+        """
+        Filter songs below a specific play count.
+        :param count: Minimum song play count
+        """
+        filtered = self.grouped_data[self.grouped_data["Count"] >= count]
+        self.grouped_data = filtered
+
+    def get_result(self):
+        """
+        Get the analyzed data
+        :return: DataFrame of the analyzed data
+        """
+        return self.grouped_data
