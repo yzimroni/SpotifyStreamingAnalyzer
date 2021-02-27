793db461a34da16290fa577d7d2bb38182825e01
Yanay Zimroni <yanay03@gmail.com>
2021-02-27 14:29:44 +0200

    Add pandas analyzing

--- analyze.py
+++ analyze.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import utils
+from spotify_streaming_analyzer import SpotifyStreamingAnalyzer
+import json
 
 
 def dir_path(string):
@@ -34,6 +37,28 @@ def run():
     parser.add_argument("--source", "-s", help="'MyData' folder from spotify data export", type=dir_path, required=True)
     parser.add_argument("--output", "-o", help="Output file", type=output_path, required=True)
     args = parser.parse_args()
+
+    print("Scanning for streaming history files...")
+    files = utils.get_streaming_history_files(args.source)
+    print("Found %i streaming history files" % len(files))
+
+    analyzer = SpotifyStreamingAnalyzer()
+    for fn in files:
+        with open(os.path.join(args.source, fn), mode="r", encoding="utf-8") as f:
+            history = json.load(f)
+            print("  == Loaded %i streaming records from %s" % (len(history), fn))
+            analyzer.add_streaming_history(history)
+
+    total_length = len(analyzer.get_streaming_history())
+    print("Total streaming records: %i" % total_length)
+    if total_length == 0:
+        print("Streaming history is empty")
+        return
+
+    result = analyzer.analyze()
+    result.to_csv(args.output)
+    print("output written to %s" % args.output)
+
 
 
 if __name__ == "__main__":
--- spotify_streaming_analyzer.py
+++ spotify_streaming_analyzer.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class SpotifyStreamingAnalyzer:
 
     def __init__(self):
@@ -9,3 +12,27 @@ class SpotifyStreamingAnalyzer:
         :param history: A list of streaming records
         """
         self.streaming.extend(history)
+
+    def get_streaming_history(self):
+        """
+        Get the streaming history list
+        :return: Current streaming history list
+        """
+        return self.streaming
+
+    def analyze(self):
+        """
+        Analyze streaming history
+        :return: DataFrame of the analyzed data
+        """
+        # Create pandas DataFrame from streaming list
+        df = pd.DataFrame(self.streaming)
+        # Group by tracks (identified by artist and track names)
+        gr = df.groupby(["artistName", "trackName"])
+        # Aggregate results
+        # Values are deviced by 1000 to convert ms to seconds
+        gr = gr.agg({"msPlayed": ["count", lambda x: int(x.sum() / 1000), lambda x: int((x.sum() / x.count()) / 1000)],
+                     "endTime": ["min", "max"]})
+        # Rename colums
+        gr.columns = ["Count", "TotalSecPlayed", "AvgSecPerPlay", "FirstPlayed", "LastPlayed"]
+        return gr
