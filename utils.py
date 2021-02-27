c832c307a6ffe8517f42a3ef657b3393fc4d37e5
Yanay Zimroni <yanay03@gmail.com>
2021-02-27 17:30:05 +0200

    Update regex

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def get_streaming_history_files(directory):
     :param directory: The directory to scan
     :return: A list containing all stream history files from the directory
     """
-    r = re.compile(r"StreamingHistory\d{1,}\.json")
+    r = re.compile(r"StreamingHistory\d{0,}\.json")
     history_files = list(filter(r.match, os.listdir(directory)))
     return history_files
 
